73fa33f85d24d438b43a22c65d7bd8aa9e51fb6f
yushin <kkad45@naver.com>
2021-07-18 19:36:52 +0900

    0718 2019년 카카오1번 해결

--- Yushin/2019년 카카오/1번/a.py
+++ Yushin/2019년 카카오/1번/a.py
@@ -1,37 +1,32 @@
 recode = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
 
 # id nickname
-# 문제풀이 실패 테스트케이스 3개성공
+
 def solution(record):
     answer = []
-
-    change = []
-
+    
+    # 딕을 이용해 아이디 변경될 떄마다 id에 관한 닉네임값 변경해주기 
     id = {}
 
     result=[]
 
     for x in record:
         arr = x.split()
+        # 들어올 떄 아이디를 변경한 경우도 처리해주기
         if arr[0] == 'Enter':
-            answer.append([arr[1],arr[1],"님이 들어왔습니다."])
+            answer.append([arr[1],"님이 들어왔습니다."])
             id[arr[1]] = arr[2]
         elif arr[0] == 'Leave':
-            answer.append([arr[0],arr[1],"님이 나갔습니다."])
-
+            answer.append([arr[1],"님이 나갔습니다."])
+        # change될 때 아이디에 대한 닉네임 변경 처리해주기
         elif arr[0] == 'Change':
-            change.append((arr[1],arr[2]))
-            
-    for i in range(len(answer)):
-        answer[i][1] = id[answer[i][1]]
-
-    for x,y  in change:
-        for i in range(len(answer)):
-            if answer[i][0] == x:
-                answer[i][1]= y
+            id[arr[1]] = arr[2]
 
     for i in range(len(answer)):
-        result.append(answer[i][1]+answer[i][2])
+        answer[i][0] = id[answer[i][0]]
+
+    for i in range(len(answer)):
+        result.append(answer[i][0]+answer[i][1])
 
     return result
 
