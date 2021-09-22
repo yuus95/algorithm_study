f8b3c5ce46f65b722645f01544e08ab1a5b70b77
yuus95 <kkad45@naver.com>
2021-09-22 18:02:08 +0900

    0922 프렌즈블록 혼자해결성공

--- Yushin/2018년 카카오/프렌즈4블록/2.py
+++ Yushin/2018년 카카오/프렌즈4블록/2.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 dx = [0,1,1]
 dy = [1,0,1]
 
@@ -7,11 +9,46 @@ dy = [1,0,1]
 def solution(m, n, board):
 
     answer = 0
-    b = list(map(list,zip(*board)))
+    b = list(map(list,board))
+
 
     while True:
+        cnt_box=[]
+        for i in range(1,m):
+            for j in range(1,n):
+                if b[i][j] != '-' and b[i][j] == b[i-1][j] == b[i-1][j-1] == b[i][j-1]:
+                    cnt_box.append((i,j))
+                    cnt_box.append((i,j-1))
+                    cnt_box.append((i-1,j-1))
+                    cnt_box.append((i-1,j))
 
-    return
+        if len(cnt_box) == 0 :
+            break
+
+        cnt_box = set(cnt_box)
+        answer += len(cnt_box)
+
+        for x,y in cnt_box:
+            b[x][y] = '-'
+        for i in range(n):
+            q = deque()
+            q.append((m-1))
+            temp = []
+            while q:
+                k =  q.popleft()
+                if b[k][i] != '-':
+                    temp.append(b[k][i])
+                if k-1 >= 0 :
+                    q.append(k-1)
+            if len(temp) != m:
+                num = m - len(temp)
+                temp += ['-'] * num
+            temp = temp[::-1]
+            for j in range(m-1,-1,-1):
+                b[j][i] = temp[j]
+
+
+    return answer
 
 
 m= [4,6]
